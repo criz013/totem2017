536055e4922b991572b6b9ce0e485ab4be541e8f
Christophe <chrastophe@gmail.com>
2017-04-06 15:14:08 +0200

    Base de donée

---
+++ totem2017.sql
@@ -0,0 +1,300 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Jeu 06 Avril 2017 à 15:10
+-- Version du serveur :  10.1.19-MariaDB
+-- Version de PHP :  5.5.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `totem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `album`
+--
+
+CREATE TABLE `album` (
+  `id` int(11) NOT NULL,
+  `id_challenge` int(11) NOT NULL,
+  `nom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alt` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `src` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `challenge`
+--
+
+CREATE TABLE `challenge` (
+  `id` int(11) NOT NULL,
+  `year` date NOT NULL,
+  `name` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hashtag` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_start` datetime NOT NULL,
+  `date_end` datetime NOT NULL,
+  `status` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `don` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_sociaux` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `revue_presse`
+--
+
+CREATE TABLE `revue_presse` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `chapo` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `corp` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lien` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `r_user_chall`
+--
+
+CREATE TABLE `r_user_chall` (
+  `id_user` int(11) NOT NULL,
+  `id_chall` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `r_user_team`
+--
+
+CREATE TABLE `r_user_team` (
+  `id_user` int(11) NOT NULL,
+  `id_team` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stat_sociaux`
+--
+
+CREATE TABLE `stat_sociaux` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `team`
+--
+
+CREATE TABLE `team` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pseudo` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avatar` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `last_name` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `first_name` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `rôle` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `status` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_users_profil` int(11) NOT NULL,
+  `token_validation` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token_lost_pwd` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users_profil`
+--
+
+CREATE TABLE `users_profil` (
+  `id` int(11) NOT NULL,
+  `name_compagny` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `logo` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `link` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `haschtag` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `avatar` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `website`
+--
+
+CREATE TABLE `website` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `url` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact` varchar(400) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `logo` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `facebook` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `twitter` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `instansgram` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_challenge` (`id_challenge`);
+
+--
+-- Index pour la table `challenge`
+--
+ALTER TABLE `challenge`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_sociaux` (`id_sociaux`);
+
+--
+-- Index pour la table `r_user_chall`
+--
+ALTER TABLE `r_user_chall`
+  ADD UNIQUE KEY `id_user` (`id_user`),
+  ADD UNIQUE KEY `id_chall` (`id_chall`),
+  ADD KEY `id_user_2` (`id_user`),
+  ADD KEY `id_chall_2` (`id_chall`);
+
+--
+-- Index pour la table `r_user_team`
+--
+ALTER TABLE `r_user_team`
+  ADD UNIQUE KEY `id_user` (`id_user`),
+  ADD UNIQUE KEY `id_team` (`id_team`);
+
+--
+-- Index pour la table `stat_sociaux`
+--
+ALTER TABLE `stat_sociaux`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `team`
+--
+ALTER TABLE `team`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_users_profil` (`id_users_profil`),
+  ADD KEY `id_users_profil_2` (`id_users_profil`);
+
+--
+-- Index pour la table `users_profil`
+--
+ALTER TABLE `users_profil`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `album`
+--
+ALTER TABLE `album`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `stat_sociaux`
+--
+ALTER TABLE `stat_sociaux`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `team`
+--
+ALTER TABLE `team`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users_profil`
+--
+ALTER TABLE `users_profil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `album`
+--
+ALTER TABLE `album`
+  ADD CONSTRAINT `album_ibfk_1` FOREIGN KEY (`id_challenge`) REFERENCES `challenge` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `challenge`
+--
+ALTER TABLE `challenge`
+  ADD CONSTRAINT `challenge_ibfk_1` FOREIGN KEY (`id_sociaux`) REFERENCES `stat_sociaux` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `r_user_chall`
+--
+ALTER TABLE `r_user_chall`
+  ADD CONSTRAINT `r_user_chall_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `r_user_chall_ibfk_2` FOREIGN KEY (`id_chall`) REFERENCES `challenge` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `r_user_team`
+--
+ALTER TABLE `r_user_team`
+  ADD CONSTRAINT `r_user_team_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `r_user_team_ibfk_2` FOREIGN KEY (`id_team`) REFERENCES `team` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_users_profil`) REFERENCES `users_profil` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
