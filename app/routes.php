e9192502197b816da289bc1f281a878f120a680d
MarcelMarseille <marcel.louis.wiel@gmail.com>
2017-04-11 09:55:10 +0100

    correction orthographe
    administrateur

--- app/routes.php
+++ app/routes.php
@@ -5,28 +5,28 @@
 		['GET', '/', 'HomePage#index', 'homePage_index'],
 
         //route pour la partie administrateur
-        ['GET|POST', '/aministrateur', 'Admin#index', 'admin_index'],
+        ['GET|POST', '/administrateur', 'Admin#index', 'admin_index'],
 
         //route pour pour qu'un benevol ou sponsor puissent modifier leurs informations
-        ['GET|POST', '/aministrateur/partenaire/[:id]', 'User#partenaire', 'user_partenaire'],
+        ['GET|POST', '/administrateur/partenaire/[:id]', 'User#partenaire', 'user_partenaire'],
 
         //Validation des inscriptions
-        ['GET|POST', '/aministrateur/gestion-inscription', 'User#inscription', 'user_inscription'],
-        ['GET|POST', '/aministrateur/gestion-inscription/detail/[:id]', 'User#inscriptionDetail', 'user_inscription_detail'],
+        ['GET|POST', '/administrateur/gestion-inscription', 'User#inscription', 'user_inscription'],
+        ['GET|POST', '/administrateur/gestion-inscription/detail/[:id]', 'User#inscriptionDetail', 'user_inscription_detail'],
 
         //Edition et suppression d'article de la revue de presse
-        ['GET', '/aministrateur/revue-presse', 'Presse#index', 'presse_index'],
-        ['GET', '/aministrateur/revue-presse/detail/[:id]', 'Presse#detail', 'presse_detail'],
+        ['GET', '/administrateur/revue-presse', 'Presse#index', 'presse_index'],
+        ['GET', '/administrateur/revue-presse/detail/[:id]', 'Presse#detail', 'presse_detail'],
 
         //Gestion des editions challenge totem
-        ['GET', '/aministrateur/gestion-edition', 'Challenge#index', 'challenge_index'],
-        ['GET', '/aministrateur/gestion-edition/detail/[:id]', 'Challenge#detail', 'challenge_detail'],
+        ['GET', '/administrateur/gestion-edition', 'Challenge#index', 'challenge_index'],
+        ['GET', '/administrateur/gestion-edition/detail/[:id]', 'Challenge#detail', 'challenge_detail'],
 
         //Gestion du site en général
-        ['GET', '/aministrateur/configuration-site', 'Web#site', 'web_site'],
-		['GET|POST', '/aministrateur/configuration-site/modification/[:id]', 'Web#modification', 'web_modification'],
+        ['GET', '/administrateur/configuration-site', 'Web#site', 'web_site'],
+		['GET|POST', '/administrateur/configuration-site/modification/[:id]', 'Web#modification', 'web_modification'],
 
-        ['GET|POST', '/aministrateur/reseau-sociaux', 'Web#sociaux', 'web_sociaux'],
+        ['GET|POST', '/administrateur/reseau-sociaux', 'Web#sociaux', 'web_sociaux'],
 
         //Identification et inscription
         ['GET|POST', '/login', 'HomePage#login', 'homePage_login'],
