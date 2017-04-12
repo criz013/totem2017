b69c424e17192f6ddfa3e9d66f284d493203d092
MarcelMarseille <marcel.louis.wiel@gmail.com>
2017-04-12 16:09:50 +0200

    Header navbar fixed
    Participer + Se connecter

--- app/Views/home-video/home-video.php
+++ app/Views/home-video/home-video.php
@@ -15,7 +15,7 @@ Fichier home-video (c'est le header de notre one page) contenant :
 */
 ?>
 
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default navbar-fixed-top">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
