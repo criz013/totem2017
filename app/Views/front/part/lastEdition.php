305f300e3f7da6202862cb6c5d744993cf8cff0e
Pascal13190 <pascalalberto.perso@gmail.com>
2017-04-13 18:08:06 +0200

    change on last edition
    css and html

--- app/Views/front/part/lastEdition.php
+++ app/Views/front/part/lastEdition.php
@@ -38,84 +38,86 @@ INCLURE LE FOOTER-SECTION
                 <div class="panel-body">
                     <!--Teaser-->
                     <!--<div class="col-sm-2 point5 ">-->
-                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                        <div class="single text-center">
-                            <div class="icon-outer">
-                                <div class="icon">
-                                    <i class="fa fa-info-circle"></i>
-                                </div><!--/icon-->
-                            </div><!--/icon-outer-->
-                            <h2>Le mot de l'orga</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, sunt, labore, vel ut et mollitia nemo cumque laboriosam odit distinctio odio obcaecati nihil maxime molestiae quam totam vitae ea voluptas!</p>
-                        </div><!--/single-->
-                    </div><!--/col-->
-                    <!--colonne1-->
-                    <!--<div class="col-sm-2 point5 ">-->
-                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-                        <div class="single text-center">
-                            <div class="icon-outer">
-                                <div class="icon">
-                                    <i class="fa fa-hashtag"></i>
-                                </div><!--/icon-->
-                            </div><!--/icon-outer-->
-                            <h2>1.2 MILLIONS</h2>
-                            <p>de likes, retweets, posts, partages en 2h ! L’impact de cette manifestation a été impressionnant: plus de 1.2 millions de personnes ont vu, aimé et partagé le hashtag #TOTEM1DPJ !</p>
-                        </div><!--/single-->
-                    </div><!--/col-->
-                    <!--colonne2-->
-                    <!--<div class="col-sm-2 point5 ">-->
-                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 ">
-                        <div class="single text-center">
-                            <div class="icon-outer">
-                                <div class="icon">
-                                    <!--<i class="fa fa-thumbs-up"></i>-->
-                                    <i class="fa fa-users"></i>
-                            </div><!--/icon-->
-                            </div><!--/icon-outer-->
-                            <h2>42 BÉNÉVOLES</h2>
-                            <p>Dynamiques, adorables et motivés !</p>
-                        </div><!--/single-->
-                    </div><!--/col-->
-                    <!--colonne3-->
-                    <!--<div class="col-sm-2 point5 ">-->
-                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 ">
-                        <div class="single text-center">
-                            <div class="icon-outer">
-                                <div class="icon">
-                                    <!--<i class="fa fa-user-plus"></i>-->
-                                    <i class="fa fa-handshake-o"></i>
-                                </div><!--/icon-->
-                            </div><!--/icon-outer-->
-                            <h2>45 PARTENAIRES</h2>
-                            <p>Pour cette 1ère édition ! Pour les structures qui ont sponsorisé des équipages, vous allez bénéficier de l’action d’Edmund !!</p>
-                        </div><!--/single-->
-                    </div><!--/col-->
-                    <!--colonne4-->
-                    <!--<div class="col-sm-2 point5 ">-->
-                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 ">
-                        <div class="single">
-                            <div class="icon-outer">
-                                <div class="icon">
-                                    <i class="fa fa-eur"></i>
-                            </div><!--/icon-->
-                            </div><!--/icon-outer-->
-                            <h2>10 300 €</h2>
-                            <p>Pour l'association 1 Déchet Par Jour / 1 Piece of Rubbish ! Suivez l’action d’1 Déchet Par Jour, la barriere humaine entre les déchets sauvages et la nature sur leur page facebook</p>
-                        </div><!--/single-->
-                    </div><!--/col-->
-                    <!--colonne5-->
-                    <!--<div class="col-sm-2 point5 ">-->
-                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 ">
-                        <div class="single">
-                            <div class="icon-outer">
-                                <div class="icon">
-                                     <i class="fa fa-car"></i>
-                                </div><!--/icon-->
-                            </div><!--/icon-outer-->
-                            <h2>26 EQUIPAGES</h2>
-                            <p>De Twizy : entreprises, institutions, associations, comédiens...Tous mobilisés car nous avons décidé de passer aux actes ..... et nous comptons bien renouveler cette belle aventure !</p>
-                        </div><!--/single-->
-                    </div><!--/col-->
+                    <div class="row row-flex">
+                            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-flex editionInfo">
+                                <div class="single text-center">
+                                    <div class="icon-outer">
+                                        <div class="icon">
+                                            <i class="fa fa-info-circle"></i>
+                                        </div><!--/icon-->
+                                    </div><!--/icon-outer-->
+                                    <h2>Le mot de l'orga</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, sunt, labore, vel ut et mollitia nemo cumque laboriosam odit distinctio odio obcaecati nihil maxime molestiae quam totam vitae ea voluptas!</p>
+                                </div><!--/single-->
+                            </div><!--/col-->
+                            <!--colonne1-->
+                            <!--<div class="col-sm-2 point5 ">-->
+                            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-flex">
+                                <div class="single text-center">
+                                    <div class="icon-outer">
+                                        <div class="icon">
+                                            <i class="fa fa-hashtag"></i>
+                                        </div><!--/icon-->
+                                    </div><!--/icon-outer-->
+                                    <h2>1.2 MILLIONS</h2>
+                                    <p>de likes, retweets, posts, partages en 2h ! L’impact de cette manifestation a été impressionnant: plus de 1.2 millions de personnes ont vu, aimé et partagé le hashtag #TOTEM1DPJ !</p>
+                                </div><!--/single-->
+                            </div><!--/col-->
+                            <!--colonne2-->
+                            <!--<div class="col-sm-2 point5 ">-->
+                            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4  col-flex">
+                                <div class="single text-center">
+                                    <div class="icon-outer">
+                                        <div class="icon">
+                                            <!--<i class="fa fa-thumbs-up"></i>-->
+                                            <i class="fa fa-users"></i>
+                                    </div><!--/icon-->
+                                    </div><!--/icon-outer-->
+                                    <h2>42 BÉNÉVOLES</h2>
+                                    <p>Dynamiques, adorables et motivés !</p>
+                                </div><!--/single-->
+                            </div><!--/col-->
+                            <!--colonne3-->
+                            <!--<div class="col-sm-2 point5 ">-->
+                            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4  col-flex">
+                                <div class="single text-center">
+                                    <div class="icon-outer">
+                                        <div class="icon">
+                                            <!--<i class="fa fa-user-plus"></i>-->
+                                            <i class="fa fa-handshake-o"></i>
+                                        </div><!--/icon-->
+                                    </div><!--/icon-outer-->
+                                    <h2>45 PARTENAIRES</h2>
+                                    <p>Pour cette 1ère édition ! Pour les structures qui ont sponsorisé des équipages, vous allez bénéficier de l’action d’Edmund !!</p>
+                                </div><!--/single-->
+                            </div><!--/col-->
+                            <!--colonne4-->
+                            <!--<div class="col-sm-2 point5 ">-->
+                            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4  col-flex">
+                                <div class="single">
+                                    <div class="icon-outer">
+                                        <div class="icon">
+                                            <i class="fa fa-eur"></i>
+                                    </div><!--/icon-->
+                                    </div><!--/icon-outer-->
+                                    <h2>10 300 €</h2>
+                                    <p>Pour l'association 1 Déchet Par Jour / 1 Piece of Rubbish ! Suivez l’action d’1 Déchet Par Jour, la barriere humaine entre les déchets sauvages et la nature sur leur page facebook</p>
+                                </div><!--/single-->
+                            </div><!--/col-->
+                            <!--colonne5-->
+                            <!--<div class="col-sm-2 point5 ">-->
+                            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4  col-flex">
+                                <div class="single">
+                                    <div class="icon-outer">
+                                        <div class="icon">
+                                             <i class="fa fa-car"></i>
+                                        </div><!--/icon-->
+                                    </div><!--/icon-outer-->
+                                    <h2>26 EQUIPAGES</h2>
+                                    <p>De Twizy : entreprises, institutions, associations, comédiens...Tous mobilisés car nous avons décidé de passer aux actes ..... et nous comptons bien renouveler cette belle aventure !</p>
+                                </div><!--/single-->
+                            </div><!--/col-->
+                    </div>
                 </div><!--/container-->
         
         		<!-- photos & partenaires edition 2016-->	
@@ -124,14 +126,14 @@ INCLURE LE FOOTER-SECTION
                         <div class="call-to-action">
                             <!--/bouton partenaires-->
                             <a class="icon" id="afficher_div" href="#!">
-                                <i class="fa fa-address-card-o center animated bounceInRigh" style="font-size:50px; color:white"><!--/icon--></i>
+                                <i class="fa fa-address-card-o center animated bounceInRigh"><!--/icon--></i>
                                 <span>Retrouvez les partenaires</span>
                                 <span>EDITION 2016</span>
                             </a>
                             
                             <!--/bouton photos-->
                             <a class="icon" href="<?php echo $this->url('lastEdition_photos') ?>">
-                                <i class="fa fa-camera center animated bounceInLeft" style="font-size:50px; color:white"><!--/icon--></i>
+                                <i class="fa fa-camera center animated bounceInLeft"><!--/icon--></i>
                                 <span>Retrouvez les photos</span>
                                 <span>EDITION 2016</span>
                             </a>
@@ -238,14 +240,14 @@ INCLURE LE FOOTER-SECTION
                         <div class="call-to-action">
                             <!--/bouton partenaires-->
                             <a class="icon" id="afficher_div" href="#!">
-                                <i class="fa fa-address-card-o center animated bounceInRigh" style="font-size:50px; color:white"><!--/icon--></i>
+                                <i class="fa fa-address-card-o center animated bounceInRigh"><!--/icon--></i>
                                 <span>Retrouvez les partenaires</span>
                                 <span>EDITION 2015</span>
                             </a>
                             
                             <!--/bouton photos-->
                             <a class="icon" href="<?php echo $this->url('lastEdition_photos') ?>">
-                                <i class="fa fa-camera center animated bounceInLeft" style="font-size:50px; color:white"><!--/icon--></i>
+                                <i class="fa fa-camera center animated bounceInLeft"><!--/icon--></i>
                                 <span>Retrouvez les photos</span>
                                 <span>EDITION 2015</span>
                             </a>
