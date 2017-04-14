01b741eb2c41e1c9896dcb544ec47bf32f78faae
MarcelMarseille <marcel.louis.wiel@gmail.com>
2017-04-14 14:57:02 +0200

    Changement navbar sup
    Se connecter descend
    Particper va direct au formulaire

--- app/Views/front/part/signin.php
+++ app/Views/front/part/signin.php
@@ -2,25 +2,25 @@
 
 <div class="row">
     <div class="container-fluid">
-        <ul id="sidenavMenu" class="nav nav-pills nav-stacked">
-            <h2 class="text-center">Participer</h2>
-            <li id="sponsortwizzy">
-                <a href="#twizzyForm">
-                    Je sponsorise le challenge en Twizzy
-                </a>
-            </li>
-            <li id="sponsor">
-                <a href="#sponsorform">
-                    Je sponsorise le challenge
-                </a>
-            </li>
-            <li id="benevole">
-                <a href="#">
-                    J'aide les organisateurs
-                </a>
-            </li>
-        </ul>
-        
+        <form action="/totem2/public/inscription" method="POST">
+            
+                <div id="useraccount" class="step step1">
+                    <?php $this->insert('front/part/signup/useraccount'); ?>
+                </div>
+                <?php /*
+                <div id="userprofile" class="step step2">
+                    <?php $this->insert('front/part/signup/userprofile'); ?>
+                </div>
+                
+                <div id="twizzyteam" class="step step3">
+                    <?php $this->insert('front/part/signup/twizzyteam'); ?>
+                </div>
+                    */?>
+                <input type="hidden" name="operation" value="signup">
+                <button type="submit" class="btn btn-success">Valider ma participation</button>
+                
+            </form>
+        <?php /*
         <div id="sidenav-content">
             
             <form action="/totem2/public/inscription" method="POST">
@@ -43,5 +43,6 @@
             </form>
           
         </div>
+        */?>
     </div>
 </div>
