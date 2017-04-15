5ff7c81e3ab6a55e61bfeb168a9830a1f610ba58
criz <chrastophe@gmail.com>
2017-04-15 18:24:59 +0200

    nouvelle bdd

--- totem2017.sql
+++ totem2017.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 06 Avril 2017 à 15:28
--- Version du serveur :  10.1.19-MariaDB
--- Version de PHP :  5.5.38
+-- Généré le :  Sam 15 Avril 2017 à 16:23
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `totem`
+-- Base de données :  `totem2017`
 --
 
 -- --------------------------------------------------------
@@ -64,7 +64,9 @@ CREATE TABLE `revue_presse` (
   `titre` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
   `chapo` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `corp` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `lien` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL
+  `lien` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dateCreate` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -123,18 +125,25 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `last_name` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
   `first_name` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `rôle` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `phone` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `created` datetime NOT NULL,
-  `modified` datetime NOT NULL,
+  `modified` datetime DEFAULT NULL,
   `status` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `id_users_profil` int(11) NOT NULL,
   `token_validation` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token_lost_pwd` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL
+  `token_lost_pwd` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `valider` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Contenu de la table `users`
+--
+
+INSERT INTO `users` (`id`, `last_name`, `first_name`, `role`, `phone`, `email`, `password`, `created`, `modified`, `status`, `token_validation`, `token_lost_pwd`, `username`, `valider`) VALUES
+(37, 'administrateur', 'administrateur', 'sponsor', '0011223344', 'administrateur@totem.fr', '$2y$10$CsGLQLu80beq2GLSC6tpduPDgf06t7/JTixgua2qLdrj3gd45aPRO', '2017-04-15 03:10:28', NULL, 'En attente', '0y_XE-h5OdgroeZMTVi-7aVsOfHiv9fp', NULL, 'administrateur@totem.fr', 0);
 
 -- --------------------------------------------------------
 
@@ -144,13 +153,21 @@ CREATE TABLE `users` (
 
 CREATE TABLE `users_profil` (
   `id` int(11) NOT NULL,
-  `name_compagny` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `logo` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `link` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `haschtag` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `avatar` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL
+  `id_users` int(11) NOT NULL,
+  `name_compagny` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
+  `logo` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `link` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `haschtag` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `avatar` varchar(250) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Contenu de la table `users_profil`
+--
+
+INSERT INTO `users_profil` (`id`, `id_users`, `name_compagny`, `description`, `logo`, `link`, `haschtag`, `avatar`) VALUES
+(5, 37, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -172,6 +189,13 @@ CREATE TABLE `website` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Contenu de la table `website`
+--
+
+INSERT INTO `website` (`id`, `titre`, `url`, `contact`, `logo`, `description`, `email`, `facebook`, `twitter`, `instansgram`) VALUES
+(1, 'Totem2', 'www.webfor3.com', '11 rue de belloi 13006 marseille', 'logo1', 'lorem lorem 2', 'chrastophe@gmail.com', 'facebook', 'twitter1', 'instagram');
+
+--
 -- Index pour les tables exportées
 --
 
@@ -188,6 +212,12 @@ ALTER TABLE `album`
 ALTER TABLE `challenge`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id_sociaux` (`id_sociaux`);
+
+--
+-- Index pour la table `revue_presse`
+--
+ALTER TABLE `revue_presse`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `r_user_chall`
@@ -221,14 +251,19 @@ ALTER TABLE `team`
 -- Index pour la table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id_users_profil` (`id_users_profil`),
-  ADD KEY `id_users_profil_2` (`id_users_profil`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `users_profil`
 --
 ALTER TABLE `users_profil`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_users` (`id_users`);
+
+--
+-- Index pour la table `website`
+--
+ALTER TABLE `website`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -239,6 +274,11 @@ ALTER TABLE `users_profil`
 -- AUTO_INCREMENT pour la table `album`
 --
 ALTER TABLE `album`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `revue_presse`
+--
+ALTER TABLE `revue_presse`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `stat_sociaux`
@@ -254,12 +294,17 @@ ALTER TABLE `team`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 --
 -- AUTO_INCREMENT pour la table `users_profil`
 --
 ALTER TABLE `users_profil`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT pour la table `website`
+--
+ALTER TABLE `website`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Contraintes pour les tables exportées
 --
@@ -291,10 +336,10 @@ ALTER TABLE `r_user_team`
   ADD CONSTRAINT `r_user_team_ibfk_2` FOREIGN KEY (`id_team`) REFERENCES `team` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Contraintes pour la table `users`
+-- Contraintes pour la table `users_profil`
 --
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_users_profil`) REFERENCES `users_profil` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `users_profil`
+  ADD CONSTRAINT `users_profil_ibfk_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
