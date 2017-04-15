e6de8f46914e9b6887d1b41d9e76222e96b0a1dd
Pascal13190 <pascalalberto.perso@gmail.com>
2017-04-15 17:42:26 +0200

    form sign and login
    so fixed et password hide/show

--- app/Views/front/part/signup/useraccount.php
+++ app/Views/front/part/signup/useraccount.php
@@ -1,5 +1,5 @@
 <div id="step1">
-    <h2>Je crée mon compte</h2>
+    <h2>Participer</h2>
 
     <div class="form-group sign">
         <label for="role" class="control-label col-sm-4">Vous voulez devenir :</label>
@@ -14,14 +14,14 @@
 		</div>
 
     <div class="form-group">
-        <label for="lastname" class="control-label col-sm-4">Nom :</label>
+        <label for="lastname" class="control-label col-sm-4 hidden-xs">Nom :</label>
         <div class="col-sm-8">
             <input type="text" name="last_name"  class="form-control" placeholder="Nom" id="lastname" required />
         </div>
     </div>
                     
     <div class="form-group">
-        <label for="firstname" class="control-label col-sm-4">Prénom :</label>
+        <label for="firstname" class="control-label col-sm-4 hidden-xs">Prénom :</label>
         <div class="col-sm-8">
             <input type="text" name="first_name" class="form-control" placeholder="Prénom" id="firstname" required />
         </div>
@@ -30,15 +30,18 @@
                     
     
     <div class="form-group">
-        <label for="email" class="control-label col-sm-4">Email :</label>
+        <label for="email" class="control-label col-sm-4 hidden-xs">Email :</label>
         <div class="col-sm-8">
+            <div class="input-group">
             <input type="text" name="email" class="form-control" placeholder="Email" id="email" required />
+            
+            </div>
         </div>
     </div>
 
 
     <div class="form-group">
-        <label for="phone" class="control-label col-sm-4">Téléphone :</label>
+        <label for="phone" class="control-label col-sm-4 hidden-xs">Téléphone :</label>
         <div class="col-sm-8">
             <input type="text" name="phone" class="form-control" placeholder="Téléphone" id="phone" required />
         </div>
@@ -47,17 +50,23 @@
 
 
     <div class="form-group">
-        <label for="password" class="control-label col-sm-4">Mot de passe :</label>
+        <label for="password" class="control-label col-sm-4 hidden-xs">Mot de passe :</label>
         <div class="col-sm-8">
-            <input type="password" name="password" class="form-control" placeholder="Mot de passe" id="password" required />
+        <div class="input-group pwd-chk">
+            <input type="password" name="password" class="form-control" placeholder="Mot de passe" id="password" required/>
+            <span class="input-group-addon" id="pwd-chk"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></span>
+        </div>
         </div>
     </div>
 
 
     <div class="form-group">
-        <label for="password_confirm" class="control-label col-sm-4">Confirmez votre mot de passe :</label>
+        <label for="password_confirm" class="control-label col-sm-4 hidden-xs">Confirmez votre mot de passe :</label>
         <div class="col-sm-8">
-            <input type="password" name="password_confirm" class="form-control"  placeholder="Vérification du mot de passe" id="password_confirma" required />
+        <div class="input-group pwd-chk">
+            <input type="password" name="password_confirm" class="form-control pwd-chk"  placeholder="Vérification du mot de passe" id="password_confirma" required /> 
+            <span class="input-group-addon" id="pwd-conf-chk"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span></span>
+        </div>
         </div>
     </div>
  
