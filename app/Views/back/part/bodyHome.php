9b778f9eb19e7746c0aee876d045c0854bd5a507
Pascal13190 <pascalalberto.perso@gmail.com>
2017-04-17 02:06:23 +0200

    more on back office
    let's style

--- app/Views/back/part/bodyHome.php
+++ app/Views/back/part/bodyHome.php
@@ -4,6 +4,61 @@
  */
 ?>
 	
-	
+			<div id="back-content" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="container">
+					<div class="row row-flex">
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-flex">
+							<div class="encart">
+								<h3><i class="fa fa-trophy" aria-hidden="true"></i> Editions</h3>
+								<div class="encart">
+									<ul id="trophy-indicators">
+											<li>A venir<span class="badge badge-vert">2</span></li>
+											<li>passée<span class="badge badge-bleu">56</span></li>
+										</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-flex">
+							<div class="encart">
+								<h3><i class="fa fa-handshake-o" aria-hidden="true"></i> Partenaires</h3>
+								<div class="encart">
+									<ul id="partenaires-indicators">
+											<li>Validé<span class="badge badge-vert">2</span></li>
+											<li>En cours d'examen<span class="badge badge-bleu">56</span></li>
+											<li>En attente<span class="badge badge-orange">81</span></li>
+											<li>Refusé<span class="badge badge-rouge">81</span></li>
+										</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-flex">
+							<div class="encart">
+								<h3><i class="fa fa-users" aria-hidden="true"></i> Bénévoles</h3>
+								<div class="encart">
+									<ul id="benevoles-indicators">
+											<li>Validé<span class="badge badge-vert">2</span></li>
+											<li>En cours d'examen<span class="badge badge-bleu">56</span></li>
+											<li>En attente<span class="badge badge-orange">81</span></li>
+											<li>Refusé<span class="badge badge-rouge">81</span></li>
+										</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-flex">
+							<div class="encart">
+								<h3><i class="fa fa-newspaper-o" aria-hidden="true"></i> Articles de presse</h3>
+								<div class="encart">
+									<ul id="presse-indicators">
+											<li>Activé<span class="badge badge-bleu">56</span></li>
+											<li>Désactivé<span class="badge badge-orange">81</span></li>
+										</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+	</div><!-- row -->
+</div><!-- main -->
 	
 		
--- app/Views/back/part/head.php
+++ app/Views/back/part/head.php
@@ -12,13 +12,15 @@
 		<!-- CSS FILES -->
     	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
     	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    	<link rel="stylesheet" href="<?= $this->assetUrl('css/fonts.css') ?>">
+		<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
     	<link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/pages/backHome.css') ?>" />
 	</head>
 	<body>
 	<header>
 		<nav id="top-infos" role="navigation">
 			<div class="navbar-header">
-				<span id="back-title">Dashboard</span>
+				<span id="back-title"><i class="fa fa-tachometer" aria-hidden="true"></i> Dashboard</span>
 				<span id="back-organisation">CHALLENGE TOTEM</span>
 				<ul id="top-indicators">
 					<li><i class="fa fa-trophy" aria-hidden="true"></i><span class="badge">2</span></li>
@@ -32,16 +34,38 @@
 		</nav>
 	</header>
 	<main>
-		<div>
-			<nav id="nav-sidebar">
-				<ul>
-					<li><a href="<?php echo $this->url("web_site")?>">Configuration du site</a></li>
-					<li><a href="<?php echo $this->url("user_inscription")?>">Inscription partenaire/Bénévole</a></li>
-					<li><a href="<?php echo $this->url("challenge_index")?>">Evènement</a></li>
-					<li><a href="<?php echo $this->url("press_index")?>">Revue de presse</a></li>
-					<li><a href="<?php echo $this->url("web_sociaux")?>">Raiseau sociaux</a></li>
-				</ul>
-			</nav>
-		</div>
+		<div id="main" class="container">
+			<div class="row">
+					<nav id="nav-sidebar" class="navbar col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						  <div class="container">
+						    
+						    <!-- Brand and toggle get grouped for better mobile display -->
+						    <div class="navbar-header">
+						      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-sidebar-collapse" aria-expanded="false" id="back-hamburger">
+						        <span class="sr-only">Toggle navigation</span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						      </button>
+						    </div>
+						    
+						    <div class="row">
+						    <!-- Collect the nav links, forms, and other content for toggling -->
+							    <div class="collapse navbar-collapse" id="nav-sidebar-collapse">
+							      <ul class="nav navbar-nav navbar-scroll">
+							        <li><a href="<?php echo $this->url("web_site")?>">Configuration du site</a></li>
+									<li><a href="<?php echo $this->url("user_inscription")?>">Inscription partenaire/Bénévole</a></li>
+									<li><a href="<?php echo $this->url("challenge_index")?>">Evènement</a></li>
+									<li><a href="<?php echo $this->url("press_index")?>">Revue de presse</a></li>
+									<li><a href="<?php echo $this->url("web_sociaux")?>">Raiseau sociaux</a></li>
+							     </ul>
+							    </div><!-- /.navbar-collapse -->
+							</div>
+
+						  </div><!-- /.container-fluid -->
+						</nav>
+				
+		
+
 		
 		
