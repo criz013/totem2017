e37fd1482a6928cd25573df86071c9ed08f69916
Alexandre Kaiser <alexandre@nouveauxterritoires.fr>
2017-04-18 23:13:27 +0200

    Ajout de la librairie/css datetime

--- app/Views/back/part/footer.php
+++ app/Views/back/part/footer.php
@@ -10,8 +10,10 @@
 
 		<!-- JAVASCRIPT FILES -->
 		<script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
-		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
 	
-		
+		<!-- need for datepicker -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
 	</body>
 </html>
