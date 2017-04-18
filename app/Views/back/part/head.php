5a8ad79f38c85133352a314d87d2868ec1e22252
Alexandre Kaiser <alexandre@nouveauxterritoires.fr>
2017-04-18 23:13:27 +0200

    Ajout de la librairie/css datetime

--- app/Views/back/part/head.php
+++ app/Views/back/part/head.php
@@ -14,7 +14,8 @@
 		<!-- CSS FILES -->
     	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
     	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
-    	<link rel="stylesheet" href="<?= $this->assetUrl('css/fonts.css') ?>">
+    	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css"/>
+		<link rel="stylesheet" href="<?= $this->assetUrl('css/fonts.css') ?>">
 		<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
     	<link rel="stylesheet" type="text/css" href="<?= $this->assetUrl('css/pages/backHome.css') ?>" />
 	</head>
