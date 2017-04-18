3383a60d96ca07158c5e58f14cb0be5cc416f102
Alexandre Kaiser <alexandre@nouveauxterritoires.fr>
2017-04-18 23:28:25 +0200

    ajout fichier de traduction fr pour datepicker

--- app/Views/back/part/footer.php
+++ app/Views/back/part/footer.php
@@ -15,5 +15,6 @@
 		<!-- need for datepicker -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/locale/fr.js"></script>
 	</body>
 </html>
