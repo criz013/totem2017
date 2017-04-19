021dc9dbe333372fcf54f6e5600337fde746b398
Pascal13190 <pascalalberto.perso@gmail.com>
2017-04-20 01:53:14 +0200

    some changes on form and css
    much better

--- app/Views/front/part/partenaireDetail.php
+++ app/Views/front/part/partenaireDetail.php
@@ -1,48 +1,47 @@
 <?php
 
-
 $objetUsersModel = new \W\Model\UsersModel;
 $tabUsers= $objetUsersModel->find($id);
 $objetUsersProfilModel = new \Model\Users_profilModel;
 $usersProfil=  $objetUsersProfilModel->search(['id_users'=>$id]);
 
 ?>
-<div class="col-xs-12 col-sm-12 col-md-8">
+<div class="container">
 
     <h1>Fiche utilisateur</h1>
     <form method="post" action="<?php echo $this->url("user_updateUser",['id'=>$id]);?>" id="fiche-user" class="form form-horizontal" enctype="multipart/form-data">
     	<div class="form-group">
             <label for="last_name" class="control-label col-sm-4 hidden-xs">Nom :</label>
             <div class="col-sm-8">
-    			<input class="form-control" type="text" id="last_name" name="last_name" value="<?php echo $tabUsers['last_name'] ?>" >
+    			<input class="form-control" type="text" id="last_name" name="last_name" placeholder="Nom" value="<?php echo $tabUsers['last_name'] ?>" required >
     		</div>
     	</div>
     	
     	<div class="form-group">
-            <label for="first_name" class="control-label col-sm-4 hidden-xs">Prenom :</label>
+            <label for="first_name" class="control-label col-sm-4 hidden-xs">Prénom :</label>
             <div class="col-sm-8">
-    			<input class="form-control" type="text" id="first_name" name="first_name" value="<?php echo $tabUsers['first_name'] ?>" >
+    			<input class="form-control" type="text" id="first_name" name="first_name" placeholder="Prénom" value="<?php echo $tabUsers['first_name'] ?>" required>
     		</div>
     	</div>
 
     	<div class="form-group">
             <label for="phone" class="control-label col-sm-4 hidden-xs">Téléphone :</label>
             <div class="col-sm-8">
-    			<input class="form-control" type="text" id="phone" name="phone" value="<?php echo $tabUsers['phone'] ?>" >
+    			<input class="form-control" type="text" id="phone" name="phone" placeholder="téléphone" value="<?php echo $tabUsers['phone'] ?>" required>
     		</div>
     	</div>
 
     	<div class="form-group">
             <label for="email" class="control-label col-sm-4 hidden-xs">E-mail :</label>
             <div class="col-sm-8">
-    			<input class="form-control" type="text" id="email" name="email" value="<?php echo $tabUsers['email'] ?>" >
+    			<input class="form-control" type="text" id="email" name="email" placeholder="e-mail" value="<?php echo $tabUsers['email'] ?>" required>
     		</div>
     	</div>
     		<input type="hidden" id="operation" name="operation" value="utilisateur" >
     		
     	<?php if($tabUsers['role'] == 'benevole' ) {?>
     	<div class="form-group">
-            <label for="avatar" class="control-label col-sm-4 hidden-xs">avatar :</label>
+            <label for="avatar" class="control-label col-sm-4 col-xs-12">Avatar ou photo :</label>
             <div class="">
     			<input class="form-control hidden" type="text" id="avatar" name="avatar" value="<?php echo $usersProfil[0]['avatar'] ?>" >
     		</div>
@@ -56,7 +55,7 @@ $usersProfil=  $objetUsersProfilModel->search(['id_users'=>$id]);
     	
     	<?php if($tabUsers['role'] == 'sponsor' ) {?>
     	<div class="form-group">
-            <label for="logo" class="control-label col-sm-4 hidden-xs">logo :</label>
+            <label for="logo" class="control-label col-sm-4 col-xs-12">Logo :</label>
             <div class="">
     			<input class="form-control hidden" type="text" id="logo" name="logo" value="<?php echo $usersProfil[0]['logo'] ?>" >
     		</div>
@@ -73,19 +72,16 @@ $usersProfil=  $objetUsersProfilModel->search(['id_users'=>$id]);
     	    </div>
     	    <div class="action">
     	        <div>
-    	        <input class="form-control col-xs-12" type="file" id="file">
+    	           <input class="form-control col-xs-12" type="file" id="file">
     	        </div>
     	        <div class="text-center">
-    	        <input type="button" class="btn btn-primary" id="btnZoomIn" value="Zoom +">
-    	        <input type="button" class="btn btn-primary" id="btnZoomOut" value="Zoom -">
+    	           <input type="button" class="btn btn-primary" id="btnZoomOut" value="Zoom -">
+                    <input type="button" class="btn btn-primary" id="btnZoomIn" value="Zoom +">
     	        </div>
-    	        <div>
-    	        <input type="button" class="btn btn-vert" id="btnCrop" value="Générer image">
+    	        <div class="text-center">
+    	           <input type="button" class="btn btn-vert" id="btnCrop" value="Générer image">
     	        </div>
     	    </div>
-	        <div class="cropped hidden">
-	
-	        </div>
     	</div>
     	
     <script src="<?= $this->assetUrl('/js/cropbox/require.js') ?>"></script>
@@ -133,57 +129,61 @@ $usersProfil=  $objetUsersProfilModel->search(['id_users'=>$id]);
 
     	<?php if($tabUsers['role'] == 'sponsor' ) {?>
     	<div class="form-group">
-            <label for="name_compagny" class="control-label col-sm-4 hidden-xs">Nom entreprise :</label>
+            <label for="name_compagny" class="control-label col-sm-4 hidden-xs">Nom de l'entreprise :</label>
             <div class="col-sm-8">
-    			<input class="form-control" type="text" id="name_compagny" name="name_compagny" value="<?php echo $usersProfil[0]['name_compagny'] ?>" >
+    			<input class="form-control" type="text" id="name_compagny" name="name_compagny" placeholder="Entreprise" value="<?php echo $usersProfil[0]['name_compagny'] ?>" required>
     		</div>
     	</div>
     	
     	<div class="form-group">
             <label for="link" class="control-label col-sm-4 hidden-xs">Site internet :</label>
             <div class="col-sm-8">
-    			<input class="form-control" type="text" id="link" name="link" value="<?php echo $usersProfil[0]['link'] ?>" >
+    			<input class="form-control" type="text" id="link" name="link" placeholder="Site internet" value="<?php echo $usersProfil[0]['link'] ?>" required >
     		</div>
     	</div>
     	
     	<div class="form-group">
             <label for="description" class="control-label col-sm-4 hidden-xs">Descriptif :</label>
             <div class="col-sm-8">
-    			<input class="form-control" type="text" id="description" name="description" value="<?php echo $usersProfil[0]['description'] ?>" >
+    			<input class="form-control" type="text" id="description" name="description" placeholder="Descriptif" value="<?php echo $usersProfil[0]['description'] ?>" required >
     		</div>
     	</div>
     	
     	<div class="form-group">
-            <label for="haschtag" class="control-label col-sm-4 hidden-xs">haschtag :</label>
+            <label for="haschtag" class="control-label col-sm-4 hidden-xs">Hashtag pour le challange :</label>
             <div class="col-sm-8">
-    			<input class="form-control" type="text" id="haschtag" name="haschtag" value="<?php echo $usersProfil[0]['haschtag'] ?>" >
+    			<input class="form-control" type="text" id="haschtag" name="haschtag" placeholder="Hashtag pour le challange" value="<?php echo $usersProfil[0]['haschtag'] ?>" required >
     		</div>
     	</div>
     	
     <?php }?>
-            <div class="form-group">
+        <div class="col-xs-12 col-sm-12 text-center">
             <button type="submit" class="btn btn-vert">Modifier</button>
+        </div>
+        <div class="col-xs-12 col-sm-12">
+            <a class="btn btn-primary" href="<?php echo $this->url('homePage_index') ?>" role="button">Retour</a>
         </div>
     </form>
 
 <?php if($tabUsers['role'] == 'sponsor' ) {?>
-    <form action="<?php echo $this->url("user_updateUser",['id'=>$id]);?>" method="post">
+    <h2>Equipage</h2>
+    <form class="form form-horizontal" action="<?php echo $this->url("user_updateUser",['id'=>$id]);?>" method="post">
         <h3>Equipier 1</h3>
     	<div class="form-group">
 	        <label for="nom_equip1" class="control-label col-sm-4 hidden-xs">Nom :</label>
 	        <div class="col-sm-8">
-				<input class="form-control" type="text" id="nom_equip1" name="nom_equip1" value="" >
+				<input class="form-control" type="text" id="nom_equip1" name="nom_equip1" placeholder="Nom équipier 1" value="" >
 			</div>
 		</div>
 		
 		<div class="form-group">
-	        <label for="prenom_equip1" class="control-label col-sm-4 hidden-xs">prenom :</label>
+	        <label for="prenom_equip1" class="control-label col-sm-4 hidden-xs">Prénom :</label>
 	        <div class="col-sm-8">
-				<input class="form-control" type="text" id="prenom_equip1" name="prenom_equip1" value="" >
+				<input class="form-control" type="text" id="prenom_equip1" name="prenom_equip1" placeholder="Prénom équipier 1" value="" >
 			</div>
 		</div>
 		
-		<div class="form-group">
+		<!-- <div class="form-group">
 	        <label for="Pseudo_equip1" class="control-label col-sm-4 hidden-xs">Pseudo :</label>
 	        <div class="col-sm-8">
 				<input class="form-control" type="text" id="Pseudo_equip1" name="Pseudo_equip1" value="" >
@@ -195,23 +195,23 @@ $usersProfil=  $objetUsersProfilModel->search(['id_users'=>$id]);
 	        <div class="col-sm-8">
 				<input class="form-control" type="text" id="avatar_equip1" name="avatar_equip1" value="" >
 			</div>
-		</div>
+		</div> -->
 		<h3>Equipier 2</h3>
         <div class="form-group">
 	        <label for="nom_equip2" class="control-label col-sm-4 hidden-xs">Nom :</label>
 	        <div class="col-sm-8">
-				<input class="form-control" type="text" id="nom_equip2" name="nom_equip2" value="" >
+				<input class="form-control" type="text" id="nom_equip2" name="nom_equip2" placeholder="Nom équipier 2" value="" >
 			</div>
 		</div>
 		
 		<div class="form-group">
-	        <label for="prenom_equip2" class="control-label col-sm-4 hidden-xs">prenom :</label>
+	        <label for="prenom_equip2" class="control-label col-sm-4 hidden-xs">Prénom :</label>
 	        <div class="col-sm-8">
-				<input class="form-control" type="text" id="prenom_equip2" name="prenom_equip2" value="" >
+				<input class="form-control" type="text" id="prenom_equip2" name="prenom_equip2" placeholder="Prénom équipier 2" value="" >
 			</div>
 		</div>
 		
-		<div class="form-group">
+		<!-- <div class="form-group">
 	        <label for="Pseudo_equip2" class="control-label col-sm-4 hidden-xs">Pseudo :</label>
 	        <div class="col-sm-8">
 				<input class="form-control" type="text" id="Pseudo_equip2" name="Pseudo_equip2" value="" >
@@ -223,13 +223,16 @@ $usersProfil=  $objetUsersProfilModel->search(['id_users'=>$id]);
 	        <div class="col-sm-8">
 				<input class="form-control" type="text" id="avatar_equip2" name="avatar_equip2" value="" >
 			</div>
-		</div>
+		</div> -->
 		<input type="hidden" id="operation" name="operation" value="pilote" >
-		<button>Ajouter</button>
-
+        <div class="col-xs-12 col-sm-12 text-center">
+		  <button class="btn btn-vert">Ajouter l'équipage</button>
+        </div>
 
     </form>
  <?php }?>
-<a class="btn btn-vert" href="<?php echo $this->url('homePage_index') ?>" role="button">Retour</a>
+    <div class="col-xs-12 col-sm-12 text-right">
+        <a class="btn btn-primary" href="<?php echo $this->url('homePage_index') ?>" role="button">Retour</a>
+    </div>
 </div>
 
