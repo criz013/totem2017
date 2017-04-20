22948f5ec61c018268e8a77725795c46cd8a083a
criz <chrastophe@gmail.com>
2017-04-20 21:21:02 +0200

    bdd

--- totem2017.sql
+++ totem2017.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.5.1
--- http://www.phpmyadmin.net
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 18 Avril 2017 à 23:44
--- Version du serveur :  5.7.11
--- Version de PHP :  7.0.4
+-- Généré le :  Jeu 20 Avril 2017 à 19:20
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -51,15 +51,16 @@ CREATE TABLE `challenge` (
   `date_end` datetime NOT NULL,
   `status` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `don` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `id_sociaux` int(11) DEFAULT NULL
+  `id_sociaux` int(11) DEFAULT NULL,
+  `uriMap` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=COMPACT;
 
 --
 -- Contenu de la table `challenge`
 --
 
-INSERT INTO `challenge` (`id`, `year`, `name`, `description`, `text`, `hashtag`, `date_start`, `date_end`, `status`, `don`, `id_sociaux`) VALUES
-(1, 2017, '', '', ' <h2>Le Programme</h2>           <p>Cette année Totem Challenge aura lieu le 22 septembre 2017</p>           <p>Départ des équipage à 09H00</p>           <p>Fin du challenge à 18H00</p>', '', '2017-04-21 09:00:00', '2017-06-19 18:00:00', '', '', NULL);
+INSERT INTO `challenge` (`id`, `year`, `name`, `description`, `text`, `hashtag`, `date_start`, `date_end`, `status`, `don`, `id_sociaux`, `uriMap`) VALUES
+(1, 2017, 'bla bla', 'bla bla', 'Le Programme           Cette année Totem Challenge aura lieu le 22 septembre 2017           Départ des équipage à 09H00           Fin du challenge à 18H00', '#hastag', '2017-04-21 09:00:00', '2017-06-19 18:00:00', 'inactive', '', NULL, '');
 
 -- --------------------------------------------------------
 
@@ -82,7 +83,11 @@ CREATE TABLE `revue_press` (
 --
 
 INSERT INTO `revue_press` (`id`, `titre`, `chapo`, `corp`, `lien`, `photo`, `dateCreate`) VALUES
-(1, 'test1', 'test1', 'test1', 'test1', NULL, '2017-04-18 09:01:18');
+(1, 'test1', 'test1', '', 'test1', 'sega1_l.png', '2017-04-18 09:01:18'),
+(2, 'test2', 'Un jolie texte pour un test.', 'Un jolie texte pour un test.', 'www.google.fr', NULL, '2017-04-19 01:02:47'),
+(3, 'Totem2', 'gtrver', 'ezftrgtr', 'rgtrgre', '', '2017-04-19 01:24:51'),
+(4, 'Totem2', 'gtrver', 'ezftrgtr', 'rgtrgre', '', '2017-04-19 01:26:15'),
+(5, 'Test creation 2', 'sgtgrg', 'rsgtrg', 'rgtrgre', '', '2017-04-19 01:27:22');
 
 -- --------------------------------------------------------
 
@@ -158,10 +163,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `last_name`, `first_name`, `role`, `phone`, `email`, `password`, `created`, `modified`, `status`, `token_validation`, `token_lost_pwd`, `username`, `valider`) VALUES
-(37, 'administrateur', 'administrateur', 'admin', '0011223344', 'administrateur@totem.fr', '$2y$10$CsGLQLu80beq2GLSC6tpduPDgf06t7/JTixgua2qLdrj3gd45aPRO', '2017-04-15 03:10:28', NULL, 'valider', '0y_XE-h5OdgroeZMTVi-7aVsOfHiv9fp', NULL, 'administrateur@totem.fr', 1),
-(39, 'bautista', 'christophhe', 'sponsor', '0011223344', 'chrastope@gmail.com', '$2y$10$ryPehVH/sCVL5F.UW/C.C.n3f/lKI1B.KyjG/7RkR1oCmo02jIPZ6', '2017-04-17 05:02:52', NULL, 'valider', 'x4VdjptHu0DE__y4JCp_msJgfDDC34dU', NULL, 'chrastope@gmail.com', 1),
-(40, 'bautista', 'christophe', 'benevole', '0011223344', 'chrastope@hotmail.com', '$2y$10$0ApbKj.e0Q6IQwtBOLx6KuxWh64AcdbHPq0Af7/c7nk70g6KGYHS.', '2017-04-18 07:09:37', NULL, 'refuser', 'Aq6xiwJf7aUdnUqTuQdtcOzyETfyssk1', NULL, 'chrastope@hotmail.com', 0),
-(41, 'test 1', 'test1', 'sponsor', '0011223344', 'test1@test.com', '$2y$10$uwbdzjN1vEGrfY7M1pGfC.YItDf7juvkv2JfsSU1iag2tSfVamvBe', '2017-04-18 10:36:45', NULL, 'En attente', 'DWeLfLMwpn7rbkM1lLYVW30BH88D1Oqs', NULL, 'test1@test.com', 0);
+(37, 'administrateur', 'administrateur', 'admin', '0011223355', 'administrateur@totem.fr', '$2y$10$CsGLQLu80beq2GLSC6tpduPDgf06t7/JTixgua2qLdrj3gd45aPRO', '2017-04-15 03:10:28', '2017-04-20 05:39:50', 'valider', '0y_XE-h5OdgroeZMTVi-7aVsOfHiv9fp', NULL, 'administrateur@totem.fr', 1);
 
 -- --------------------------------------------------------
 
@@ -185,10 +187,7 @@ CREATE TABLE `users_profil` (
 --
 
 INSERT INTO `users_profil` (`id`, `id_users`, `name_compagny`, `description`, `logo`, `link`, `haschtag`, `avatar`) VALUES
-(5, 37, NULL, NULL, NULL, NULL, NULL, NULL),
-(7, 39, NULL, NULL, NULL, NULL, NULL, NULL),
-(8, 40, NULL, NULL, NULL, NULL, NULL, NULL),
-(9, 41, NULL, NULL, NULL, NULL, NULL, NULL);
+(5, 37, '', '', 'logoavatar-id-37.png', '', '', 'logoavatar-id-37.png');
 
 -- --------------------------------------------------------
 
@@ -217,7 +216,7 @@ CREATE TABLE `website` (
 --
 
 INSERT INTO `website` (`id`, `titre`, `url`, `adresse`, `codePostal`, `ville`, `phone`, `logo`, `description`, `email`, `facebook`, `twitter`, `instansgram`) VALUES
-(1, 'Challenge Totem 2017', 'www.webfor3.com', '9 impasse des gymnastes', '13012', 'Marseille', '', 'logo1', 'lorem lorem 2', 'chrastophe@gmail.com', 'https://www.facebook.com/christophe.bautista.7', 'https://twitter.com/crizzouille', 'https://twitter.com/crizzouille');
+(1, 'Challenge Totem 2017', 'www.webfor3.com', '9 impasse des gymnastes', '13012', 'Marseille', '0011223344', 'logo.gif', 'gogol', 'chrastophe@gmail.com', 'https://www.facebook.com/christophe.bautista.7', 'https://twitter.com/crizzouille', 'https://twitter.com/crizzouille');
 
 --
 -- Index pour les tables exportées
@@ -303,12 +302,12 @@ ALTER TABLE `album`
 -- AUTO_INCREMENT pour la table `challenge`
 --
 ALTER TABLE `challenge`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT pour la table `revue_press`
 --
 ALTER TABLE `revue_press`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT pour la table `stat_sociaux`
 --
@@ -323,12 +322,12 @@ ALTER TABLE `team`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 --
 -- AUTO_INCREMENT pour la table `users_profil`
 --
 ALTER TABLE `users_profil`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 --
 -- AUTO_INCREMENT pour la table `website`
 --
